9cdab9fc6d9cda9954ee53041cfb70d575ad902f
Jin <hanj7221@gmail.com>
2020-10-26 12:14:00 -0400

    fixing printouts

--- agent.py
+++ agent.py
@@ -153,6 +153,7 @@ if __name__ == "__main__":
         # Upload to cache and set __cached__ to true
         input_file_set_dir = os.listdir()[0]
         if os.path.isdir:
+            print('copying %s to %s', input_file_set_dir, os.path.join(workspace, project_id, input_file_set_dir))
             copy_tree(input_file_set_dir, os.path.join(workspace, project_id, input_file_set_dir))
         # Meta.update_file_set_meta(input_file_set, [], {'__cached__' : True})
 
